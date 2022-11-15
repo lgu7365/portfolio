0c1408b6d9917681371b45fd163790c8f3e4d99f
Leegeonu <lgu_lgu@naver.com>
2022-11-16 01:05:52 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import { Link } from 'react-router-dom';
+
+function Header() {
+  const [btn, setBtn] = useState(false);
+
+  const toggle = () => {
+    setBtn(!btn);
+  }
+  return (
+    <header>
+      <div className="name">
+        <Link to="home">
+          <div className="header_title">LGU's Portpolio</div>
+        </Link>
+      </div>
+      <ul className={"navbar" + (btn ? ' open' : '')}>
+        <li><Link to="aboutme">About me</Link></li>
+        <li><Link to="skills">Skills</Link></li>
+        <li><Link to="project">Project</Link></li>
+        <li><Link to="contact">Contact</Link></li>
+      </ul>
+      <div className="list" onClick={toggle}><i className="fa-solid fa-bars" /></div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="title">
+        안녕하세요. <br /> 
+        프론트엔드 개발자 <br />
+        <strong>이건우</strong> 입니다.
+      </div>
+    </div>
+  );
+}
+
+export default Home;
